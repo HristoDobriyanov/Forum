1ef717bf84f614a5f7070dd2194183013b0eaf1e
Hristo Dobriyanov <hdobriyanov@gmail.com>
2019-03-03 13:23:55 +0200

    Initial Commit

---
+++ Forum/Data/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Forum.Data.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+}
